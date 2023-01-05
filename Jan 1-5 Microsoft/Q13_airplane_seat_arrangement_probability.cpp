2ebadbc44b60aa1270a966003d9263b45e41e392
Vijay-CPP <vijaytamada02@gmail.com>
2023-01-05 23:30:50 +0530

    Solved Q13

---
+++ Jan 1-5 Microsoft/Q13_airplane_seat_arrangement_probability.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define v vector
+
+class Solution
+{
+public:
+    double nthPersonGetsNthSeat(int n)
+    {
+        return n == 1 ? 1 : 0.5;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
